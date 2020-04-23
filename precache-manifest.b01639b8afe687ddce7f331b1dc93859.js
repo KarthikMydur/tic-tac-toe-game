5697b1b877ba8f203456155e6de64c113cfba270
KarthikMydur <30320307+KarthikMydur@users.noreply.github.com>
2020-04-23 14:11:37 +0530

    Updates

---
+++ precache-manifest.b01639b8afe687ddce7f331b1dc93859.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dc69beac38361676d8e59db1a48950",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "f109eca1f06f34f0ccf5",
+    "url": "/tic-tac-toe-game/static/css/2.b21ae178.chunk.css"
+  },
+  {
+    "revision": "4c2b2d359deca80bdcc4",
+    "url": "/tic-tac-toe-game/static/css/main.bb69310d.chunk.css"
+  },
+  {
+    "revision": "f109eca1f06f34f0ccf5",
+    "url": "/tic-tac-toe-game/static/js/2.cc25bb06.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe-game/static/js/2.cc25bb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2b2d359deca80bdcc4",
+    "url": "/tic-tac-toe-game/static/js/main.325a0596.chunk.js"
+  },
+  {
+    "revision": "cf16e7efaf679a79cf9f",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.7c1f43ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
